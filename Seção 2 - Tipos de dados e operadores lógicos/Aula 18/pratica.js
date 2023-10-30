9a0b839540bfa3dde6ac39883b944d106b180495
Josias <josiassalermojunior>
2023-10-30 05:11:16 -0300

    aula 18

---
+++ Seção 2 - Tipos de dados e operadores lógicos/Aula 18/pratica.js
@@ -0,0 +1,8 @@
+/** Aula 18: Tipos de dado: number */
+console.log(5);
+console.log(12.7);
+console.log(-123);
+
+console.log(typeof 5);
+console.log(typeof 12.7);
+console.log(typeof -123);
---
+++ Seção 2 - Tipos de dados e operadores lógicos/Aula 18/slide.js
@@ -0,0 +1,4 @@
+/** Aula 18: Tipos de dado: number */
+console.log(typeof 13);
+console.log(typeof 1.8);
+console.log(typeof -5);
