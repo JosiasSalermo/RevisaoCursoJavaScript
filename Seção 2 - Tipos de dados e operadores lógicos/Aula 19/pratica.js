f21908bedad2e5341c2f0de3bdcd913717a82020
Josias <josiassalermojunior>
2023-11-02 05:33:12 -0300

    aula 19 Operações aritméticas com number

--- Seção 2 - Tipos de dados e operadores lógicos/Aula 19/pratica.js
+++ Seção 2 - Tipos de dados e operadores lógicos/Aula 19/pratica.js
@@ -1,1 +1,25 @@
 /** Aula 19: Operações aritméticas com number */
+
+// Soma
+console.log(2 + 2);
+
+// Subtração
+console.log(5 - 12);
+
+// Multiplicação
+console.log(4 * 4);
+
+// Divisão
+console.log(5 / 3);
+
+// Módulo
+console.log(20 % 2);
+console.log(21 % 2);
+
+// Saber o tipo => typeof
+console.log(typeof (2+3));
+console.log(typeof (5 - 12));
+console.log(typeof (4 * 4));
+console.log(typeof (5 / 3));
+console.log(typeof (20 % 2));
+console.log(typeof (21 % 2));
--- Seção 2 - Tipos de dados e operadores lógicos/Aula 19/slide.js
+++ Seção 2 - Tipos de dados e operadores lógicos/Aula 19/slide.js
@@ -1,1 +1,17 @@
 /** Aula 19: Operações aritméticas com number */
+/**
+ * Operação mais feita com os números em JS
+ * E o resultado da operação aritmética produz um novo Number
+ */
+
+// Soma
+console.log(2 + 2);
+
+// Subtração e Multiplicação
+console.log(2 * 4 - 3);
+
+// Divisão
+console.log(8 / 4);
+
+// Ordem de precedencia Funciona com a mesma da matemática
+console.log(5+ (2*4));
