47295292a4b5f681df749e23f869045e60cc0dee
Josias <josiassalermojunior>
2023-11-02 05:19:05 -0300

    aula 18 typeof

--- Seção 2 - Tipos de dados e operadores lógicos/Aula 18/pratica.js
+++ Seção 2 - Tipos de dados e operadores lógicos/Aula 18/pratica.js
@@ -3,6 +3,7 @@ console.log(5);
 console.log(12.7);
 console.log(-123);
 
+// Verificar tipo => typeof
 console.log(typeof 5);
 console.log(typeof 12.7);
 console.log(typeof -123);
--- Seção 2 - Tipos de dados e operadores lógicos/Aula 18/slide.js
+++ Seção 2 - Tipos de dados e operadores lógicos/Aula 18/slide.js
@@ -1,4 +1,6 @@
 /** Aula 18: Tipos de dado: number */
+
+// Verificar tipo => typeof
 console.log(typeof 13);
 console.log(typeof 1.8);
 console.log(typeof -5);
