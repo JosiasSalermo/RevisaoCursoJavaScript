f000df4ebd5157409e5f4ed47f564296abf32906
Josias <josiassalermojunior>
2023-11-03 19:53:35 -0300

    aula 20 Numbers: Special Numbers

---
+++ Seção 2 - Tipos de dados e operadores lógicos/Aula 20/pratica.js
@@ -0,0 +1,9 @@
+/** Aula 20: Numbers: Special Numbers */
+console.log(Infinity);
+console.log(-Infinity);
+console.log(NaN);
+
+// Tipo
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(typeof NaN);
---
+++ Seção 2 - Tipos de dados e operadores lógicos/Aula 20/slide.js
@@ -0,0 +1,8 @@
+/** Aula 20: Numbers: Special Numbers */
+/**
+ * Considerados números, mas não funcionam como números
+ * Infinity
+ * -Infinity
+ * NaN (Not A Number)
+ */
+
