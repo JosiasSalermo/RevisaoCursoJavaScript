0774cb0898e5fa2769221271ff61987a90572e09
Josias <josiassalermojunior>
2023-11-04 04:43:30 -0300

    aula 21 Tipo de dado: String

---
+++ Seção 2 - Tipos de dados e operadores lógicos/Aula 21/pratica.js
@@ -0,0 +1,22 @@
+/** Aula 21: Tipo de dado: String */
+
+// Aspas duplas
+console.log("Oi, meu nome é Matheus");
+
+
+// Aspas simples
+console.log('Eu queria comprar um carro.')
+
+
+// Literal String
+console.log(`Teste`);
+
+console.log("Infinity");
+console.log(Infinity);
+
+// Saber Tipo
+console.log(typeof "Oi, meu nome é Matheus");
+console.log(typeof 'Eu queria comprar um carro');
+console.log(typeof `Teste`);
+console.log(typeof "Infinity");
+console.log(typeof Infinity);
---
+++ Seção 2 - Tipos de dados e operadores lógicos/Aula 21/slide.js
@@ -0,0 +1,10 @@
+/** Aula 21: Tipo de dado: String */
+
+// Com aspas simples
+console.log(typeof 'Isso é uma String');
+
+// Com aspas Duplas
+console.log(typeof "Este texto aqui também");
+
+// com Template literals
+console.log(`E este também`); 
